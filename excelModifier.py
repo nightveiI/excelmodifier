975713223ad65766f3d1f5b8ba8739a9baaf69ff
Jose Aguilar <jhagui7464@gmail.com>
2021-12-04 13:59:57 -0600

    test

--- excelModifier.py
+++ excelModifier.py
@@ -1,5 +1,9 @@
 import xlwings as xw
 from datetime import date
+import pandas as pd
+
+read_file = pd.read_csv (r'Path where the CSV file is stored\File name.csv')
+read_file.to_excel (r'Path to store the Excel file\File name.xlsx', index = None, header=True)
 
 today = date.today()
 
