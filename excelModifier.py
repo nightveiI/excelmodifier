f1a749539f4c48b06e2dacc5c2e1954503542156
Jose Aguilar <jhagui7464@gmail.com>
2021-12-22 18:06:51 -0600

    added better settings options, began conversion settings to work to a degree will debug later

--- excelModifier.py
+++ excelModifier.py
@@ -3,8 +3,8 @@ from datetime import date
 import pandas as pd
 
 xw.App(visible=False)
-read_file = pd.read_csv (r'D:\test\DF_Standard_Project_Export.csv')
-read_file.to_excel (r'D:\test\DF_Standard_Project_Export.xlsx', index = None, header=True)
+# = pd.read_csv (r'D:\test\DF_Standard_Project_Export.csv')
+#read_file.to_excel (r'D:\test\DF_Standard_Project_Export.xlsx', index = None, header=True)
 
 today = date.today()
 
@@ -42,8 +42,10 @@ def lastRow(idx, workbook, col=1):
 # the sheets we are going to be using are, from the new workbook: general, steep, and low slope
 # and the dataforma export which is all jumbled together with the other columns we don't want for the current data analysis
 
+file = r'C:\Users\jhagu\Downloads\DF_Standard_Project_Export(5).csv'
+file.replace('.csv', '.xlsx')
 newWB = xw.Book()
-dfWB = xw.Book(r'D:\test\DF_Standard_Project_Export.xlsx')
+dfWB = xw.Book(file)
 
 
 newWB.sheets[0].name = 'Steep Slope'
@@ -58,10 +60,13 @@ lowSlope = newWB.sheets['Low Slope']
 steepSlope = newWB.sheets['Steep Slope']
 
 print("Creating new workbook")
+indexes = ["Project ID", "Number Alt", "Project Name", "Building Name", "Building Address", "Building City", "Building State", "Building Zip", "Assigned To", "Type", "Subtype", "Status", "Status Date", "Who Created", "Date Created", "Modified", "Who Modified", "Bid Amount",  "Original Contract Amount", "Revised Contract Amount", "Outstanding Contract Amount", "Gross Profit Margin %", "Notes", "Actual Project Cost", "Actual Project Cost Who", "Actual Project Cost Date", "Source", "Budget Amount", "Budget Notes", "Budget Dates", "Contract With Object", "Contract With Name", "Contract With Office Name", "Salesperson Name",
+           "Contract Terms", "Contract Term Notes", "Division", "Reference", "Subsource", "Client PO Number", "Local Union", "Construction Capacity", "Exclusions", "Special Instructions", "Contract Date", "Hide Daily Work Crew", "Running Notes", "Reference Notes", "Serial Number", "Production Status", "Contract Status", "Contract Status Date Open", "Contract Status Date Completed", "Contract Status Date Closed", "Contract Status Who Open", "Contract Status Who Completed", "Contract Status Who Closed", "Production Status Date", "Status Who", "Production Status Who", "Contact Name", "Contact Phone Number", "Contact Email"]
+
 
 
 # we copy the original data forma into an array we will use later
-generalArr = dfWB.sheets[0].range('A:BK').value
+generalArr =  dfWB.sheets[0].range((1, 1), (lastRow(0, dfWB), len(indexes))).value
 
 print("Copying headers")
 
